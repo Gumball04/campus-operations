8da73052b203b458346dacf6fbbd522b41abab41
Amr Shadid <amroshadid@gmail.com>
2026-06-20 20:41:57 +0300

    initialization

---
+++ src/main/java/com/amr/campus_operations_ai/CampusOperationsAiApplication.java
@@ -0,0 +1,13 @@
+package com.amr.campus_operations_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOperationsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusOperationsAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amr/campus_operations_ai/CampusOperationsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amr.campus_operations_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusOperationsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
